ec38527568c8c6bcb962bbbe375842c612aadfeb
FilipSari <filipsari@gmail.com>
2021-08-10 22:17:16 +0200

    JSX intro

---
+++ Intro React/JSX/index.js
@@ -0,0 +1,31 @@
+/******************************* */
+
+
+// JSX looks very similar to HTML:
+<ul>
+   <li>1 cup Fresh Basil</li>
+   <li>1 cup Pine Nuts</li>
+   <li>2 cups Butter Lettuce</li>
+   <li>1 Yellow Squash</li>
+   <li>1/2 cup Olive Oil</li>
+   <li>3 cloves of Garlic</li>
+</ul>
+
+/* This is also JSX:
+
+<IngredientsList>
+    <Ingredient />
+    <Ingredient />
+    <Ingredient />
+</IngredientsList>  */
+
+//JSX is JavaScript, so you can incorporate JSX directly inside of JavaScript functions.
+
+ /* <ul>
+   {this.props.ingredients.map((ingredient, i) =>
+       <li key={i}>{ingredient}</li>
+   )}
+</ul>  */
+
+
+
